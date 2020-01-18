4a917438b50e8ab5a323c7d4e3f081262ce253f7
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-01-18 09:40:46 +0800

    update challenge

---
+++ Computer programming/Intro to JS-Drawing & Animation/Animation Basics/explodingSun.js
@@ -0,0 +1,18 @@
+noStroke();
+// the beautiful blue sky
+background(82, 222, 240);
+
+// the starting size for the sun
+var sunSize = 34;
+
+draw = function() {
+  // The sun, a little circle on the horizon
+  fill(255, 204, 0);
+  ellipse(200, 298, sunSize, sunSize);
+
+  // The land, blocking half of the sun
+  fill(76, 168, 67);
+  rect(0, 300, 400, 100);
+
+  sunSize = sunSize + 1;
+};
