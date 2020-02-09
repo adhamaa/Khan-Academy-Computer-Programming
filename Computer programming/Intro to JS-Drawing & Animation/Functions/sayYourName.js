8e49d966e1a3b0ab5cb65915cfc92b1aafde4af4
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-02-10 05:24:02 +0800

    Create sayYourName.js

---
+++ Computer programming/Intro to JS-Drawing & Animation/Functions/sayYourName.js
@@ -0,0 +1,13 @@
+var greeting = function() {
+  var textX = random(0, 300);
+  var textY = random(0, 300);
+  var yourName = "ADHAMAA";
+
+  fill(255, 0, 0);
+  textSize(30);
+  text("Hiiii, " + yourName, textX, textY);
+};
+greeting();
+greeting();
+greeting();
+greeting();
